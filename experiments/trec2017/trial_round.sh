c95e6e1fad2de9ae72d4d16dc21a8bc43cabad72
José Devezas <joseluisdevezas@gmail.com>
2018-02-15 13:53:08 +0000

    Removed experiments directory. This is now maintained separately in a personal DokuWiki regarding my own work.

--- experiments/trec2017/trial_round.sh
+++
@@ -1,1 +0,0 @@
-time ./army-ant.py evaluation --index-location localhost:8182/goe_trec2017 --index-type goe_csv --eval-format ll-api --base-url http://api.trec-open-search.org --api-key 6E6B10EB18D56CAE-4WHODA8IRV121MNV --run-id goe_trec2017
