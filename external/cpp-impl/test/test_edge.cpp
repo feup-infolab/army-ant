14d14226134cc80ade702203d535ce6b43c2fc3b
José Devezas <joseluisdevezas@gmail.com>
2018-03-14 18:34:00 +0000

    Trying to fix issue with comparison of unordered_set of shared_ptr.

--- external/cpp-impl/test/test_edge.cpp
+++ external/cpp-impl/test/test_edge.cpp
@@ -17,10 +17,11 @@ int testEdgeCompare() {
     boost::shared_ptr<Edge> edge1 = boost::make_shared<DocumentEdge>(
             NodeSet({boost::make_shared<DocumentNode>("n0"), boost::make_shared<TermNode>("n1")}),
             NodeSet({boost::make_shared<EntityNode>("n2")}));
+
     boost::shared_ptr<Edge> edge2 = boost::make_shared<DocumentEdge>(
-            NodeSet({boost::make_shared<DocumentNode>("n0"),
-                     boost::make_shared<TermNode>("n1")}),
+            NodeSet({boost::make_shared<DocumentNode>("n0"), boost::make_shared<TermNode>("n1")}),
             NodeSet({boost::make_shared<EntityNode>("n2")}));
+
     std::cout << "*edge1 == *edge2: " << (*edge1 == *edge2) << std::endl;
     if (*edge1 == *edge2) return 0;
     return 1;
--- external/cpp-impl/test/test_node.cpp
+++ external/cpp-impl/test/test_node.cpp
@@ -13,19 +13,47 @@
 
 int testNodeCompare() {
     std::cout << "\n==> Testing Node compare" << std::endl;
+
     boost::shared_ptr<Node> node1 = boost::make_shared<DocumentNode>("n1");
     boost::shared_ptr<Node> node2 = boost::make_shared<DocumentNode>("n1");
+
     std::cout << "node1 == node2: " << (node1 == node2) << std::endl;
     std::cout << "*node1 == *node2: " << (*node1 == *node2) << std::endl;
+
     if (*node1 == *node2) return 0;
     return 1;
 }
 
+struct NEqual : std::binary_function<boost::shared_ptr<int>, boost::shared_ptr<int>, bool> {
+    bool operator()(const boost::shared_ptr<int> &lhs, const boost::shared_ptr<int> &rhs) const {
+        return *lhs == *rhs;
+    };
+};
+
+struct NHash : std::unary_function<boost::shared_ptr<int>, std::size_t> {
+    std::size_t operator()(const boost::shared_ptr<int> &i) const {
+        return boost::hash<int>()(*i);
+    }
+};
+
 int testNodeSetCompare() {
     std::cout << "\n==> Testing NodeSet compare" << std::endl;
+
+    /*std::cout << "generic set compare: "
+              << (boost::unordered_set<boost::shared_ptr<int>, NEqual, NHash, std::allocator<int>>(
+                      {boost::make_shared<int>(10), boost::make_shared<int>(20)}) ==
+                  boost::unordered_set<boost::shared_ptr<int>, NEqual, NHash, std::allocator<int>>(
+                          {boost::make_shared<int>(10), boost::make_shared<int>(20)}))
+              << std::endl;*/
+
     NodeSet nodeSet1({boost::make_shared<DocumentNode>("n1"), boost::make_shared<TermNode>("n2")});
     NodeSet nodeSet2({boost::make_shared<DocumentNode>("n1"), boost::make_shared<TermNode>("n2")});
+
+    std::cout << nodeSet1.size() << std::endl;
+    std::cout << (**nodeSet1.begin()) << std::endl;
+
     std::cout << "nodeSet1 == nodeSet2: " << (nodeSet1 == nodeSet2) << std::endl;
+
     if (nodeSet1 == nodeSet2) return 0;
     return 1;
 }
@@ -35,8 +63,8 @@ int main(int argc, char **argv) {
 
     int ret;
 
-    if ( (ret = testNodeCompare()) != 0) return ret;
-    if ( (ret = testNodeSetCompare()) != 0) return ret;
+    if ((ret = testNodeCompare()) != 0) return ret;
+    if ((ret = testNodeSetCompare()) != 0) return ret;
 
     return 0;
 }
