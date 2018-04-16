0c4747f3bc954503611eaf37094a57305c1d8b4f
José Devezas <joseluisdevezas@gmail.com>
2018-04-16 15:43:02 +0100

    Created a more generic function exploration script.

--- util/graph-analysis/functions.R
+++ util/graph-analysis/functions.R
@@ -14,6 +14,10 @@
 source('setup.R')
 library(shiny)
 
+#
+# IDF Functions
+#
+
 sigmoid_idf <- function(N, n) sigmoid((N^-1)*(length(n)-n)/n)*2-1
 
 prob_idf <- function(N, n) log10((N - n) / n)
@@ -23,22 +27,34 @@ idf_funcs <- list(
   Probabilistic=prob_idf
 )
 
+#
+# Common
+#
+
+plot_funcs <- function(funcs, params) {
+  data <- data.frame(n=n)
+  data <- do.call(rbind, lapply(names(funcs), function(func_name) {
+    cbind(data, func=func_name, val=do.call(funcs[[func_name]], params))
+  }))
+  
+  ggplot(data, aes(x=n, y=val, color = func)) +
+    geom_line(size=1.1) +
+    scale_color_discrete("Variant") +
+    xlab(expression(n[t])) +
+    ylab("IDF") +
+    xlim(0, N) +
+    ylim(0, 1) +
+    theme(legend.position = 'top')  
+}
+
+#
+# Main
+#
+
 # Corpus size
 N <- 2200
 
 # Random uniform simulation of the number of documents a term appears in (not realistic)
 n <- sample(seq(0, N), 1e5, replace = T)
 
-data <- data.frame(n=n)
-data <- do.call(rbind, lapply(names(idf_funcs), function(idf_func_name) {
-  cbind(data, func=idf_func_name, val=idf_funcs[[idf_func_name]](N, n))
-}))
-
-ggplot(data, aes(x=n, y=val, color = func)) +
-  geom_line(size=1.1) +
-  scale_color_discrete("Variant") +
-  xlab(expression(n[t])) +
-  ylab("IDF") +
-  xlim(0, N) +
-  ylim(0, 1) +
-  theme(legend.position = 'top')
+plot_funcs(idf_funcs, list(N=N, n=n))
