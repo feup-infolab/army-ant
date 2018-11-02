23b4d3fdf4f4f65a1a4cad981e1c542d81bb5d51
José Devezas <joseluisdevezas@gmail.com>
2018-11-02 11:17:58 +0000

    Added runs for HGoE representation model revision based on INEX 2009 10T-NL.

---
+++ runs/inex-10t-nl/hgoe-repr_revision/07-index-hgoe_with_related_to_by_subj.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+time ./army-ant.py index \
+    --source-path "/opt/army-ant/collections/inex-2009-10t-nl/corpus" \
+    --source-reader "inex_dir" \
+    --index-location "/opt/army-ant/indexes/inex-2009-10t-nl/hgoe-dbpedia-with_related_to_by_subj" \
+    --index-type "hgoe:related_to_by_subj"
