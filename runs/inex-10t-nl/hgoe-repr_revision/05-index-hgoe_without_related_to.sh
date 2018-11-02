b10efcf0bc6f1ce39124bc20a5480ca46c43e764
José Devezas <joseluisdevezas@gmail.com>
2018-11-02 12:08:22 +0000

    Renamed index directory, which for this collection, does not have the option to enable or disable DBpedia.

--- runs/inex-10t-nl/hgoe-repr_revision/05-index-hgoe_without_related_to.sh
+++ runs/inex-10t-nl/hgoe-repr_revision/05-index-hgoe_without_related_to.sh
@@ -3,5 +3,5 @@
 time ./army-ant.py index \
     --source-path "/opt/army-ant/collections/inex-2009-10t-nl/corpus" \
     --source-reader "inex_dir" \
-    --index-location "/opt/army-ant/indexes/inex-2009-10t-nl/hgoe-dbpedia-without_related_to" \
+    --index-location "/opt/army-ant/indexes/inex-2009-10t-nl/hgoe-without_related_to" \
     --index-type "hgoe:skip_related_to"
--- runs/inex-10t-nl/hgoe-repr_revision/07-index-hgoe_with_related_to_by_subj.sh
+++ runs/inex-10t-nl/hgoe-repr_revision/07-index-hgoe_with_related_to_by_subj.sh
@@ -3,5 +3,5 @@
 time ./army-ant.py index \
     --source-path "/opt/army-ant/collections/inex-2009-10t-nl/corpus" \
     --source-reader "inex_dir" \
-    --index-location "/opt/army-ant/indexes/inex-2009-10t-nl/hgoe-dbpedia-with_related_to_by_subj" \
+    --index-location "/opt/army-ant/indexes/inex-2009-10t-nl/hgoe-with_related_to_by_subj" \
     --index-type "hgoe:related_to_by_subj"
