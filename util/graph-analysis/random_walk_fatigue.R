9f5ec0ad1bdad2dbcdc7bcd102b82d9c30bd9269
José Devezas <joseluisdevezas@gmail.com>
2018-11-09 11:32:21 +0000

    Now loading Amazong Meta graph (preparation).

--- util/graph-analysis/random_walk_fatigue.R
+++ util/graph-analysis/random_walk_fatigue.R
@@ -4,4 +4,6 @@ pacman::p_load(
   igraph
 )
 
-g <- read.graph("~/Downloads/example.net", format = "pajek")
+graph_path <- gzfile("/opt/army-ant/output/amazon-meta-simnet.gml.gz")
+g <- read.graph(graph_path, format = "gml")
+V(g)$page_rank <- page_rank(g, directed = FALSE)$vector
