cfaf5c7a37470abde896f22f1c9591bb3e89a314
José Devezas <joseluisdevezas@gmail.com>
2018-11-09 13:38:59 +0000

    Devised the base experiment for a Fatigued PageRank.

--- util/graph-analysis/random_walk_fatigue.R
+++ util/graph-analysis/random_walk_fatigue.R
@@ -4,6 +4,14 @@ pacman::p_load(
   igraph
 )
 
+fatigued_page_rank <- function(g) {
+  0 # TODO
+}
+
 graph_path <- gzfile("/opt/army-ant/output/amazon-meta-simnet.gml.gz")
 g <- read.graph(graph_path, format = "gml")
 V(g)$page_rank <- page_rank(g, directed = FALSE)$vector
+V(g)$fatigued_page_rank <- fatigued_page_rank(g)
+
+# TODO Extract ranks before correlating?
+cor(V(g)$page_rank, V(g)$fatigued_page_rank, method = "spearman")
