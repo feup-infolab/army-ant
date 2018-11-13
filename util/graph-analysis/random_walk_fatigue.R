18be49dd5832bcaa3182af43bb5a15c835c63206
José Devezas <joseluisdevezas@gmail.com>
2018-11-13 16:58:44 +0000

    Started working on power iteration method for Fatigued PageRank.

--- util/graph-analysis/random_walk_fatigue.R
+++ util/graph-analysis/random_walk_fatigue.R
@@ -73,7 +73,7 @@ page_rank_power_iteration <- function(g, d=0.85, eps=0.0001) {
   v <- as.matrix(runif(N))
   v <- v / norm(v)
   last_v <- matrix(1, N) * 100
-  M_hat <- d * M + (1 - d) / N * matrix(1, N, N)
+  M_hat <- d * M + (1-d)/N * matrix(1, N, N)
 
   iter <- 0
   repeat {
@@ -104,10 +104,10 @@ fatigued_transition_probability <- function(nf, method="zero", ...) {
   eval(parse(text=sprintf("%s(...)", method)))
 }
 
-fatigued_page_rank_simulation <- function(g, d=0.85, steps=1000, PR = NULL, NF = NULL) {
+fatigued_page_rank_simulation <- function(g, d=0.85, nf=10, steps=1000, PR = NULL, NF = NULL) {
   for (i in 1:vcount(g)) {
     cat("==> Walking", steps, "random steps from node", i, "\n")
-    PR <- fatigued_page_rank_simulation_iter(g, i, d=d, steps=steps, PR = PR, NF = NF)
+    PR <- fatigued_page_rank_simulation_iter(g, i, d=d, nf=nf, steps=steps, PR = PR, NF = NF)
   }
   
   list(
@@ -118,7 +118,7 @@ fatigued_page_rank_simulation <- function(g, d=0.85, steps=1000, PR = NULL, NF =
 
 # Note: In RWS, there is no teleport, so the process would end when no outgoing vertices are available.
 # This means that we cannot directly compare FPR with RWS.
-fatigued_page_rank_simulation_iter <- function(g, start, d, steps, PR = NULL, NF = NULL) {
+fatigued_page_rank_simulation_iter <- function(g, start, d, nf, steps, PR = NULL, NF = NULL) {
   if (is.null(PR)) PR <- rep(0, vcount(g))
   if (is.null(NF)) NF <- rep(0, vcount(g))
   
@@ -153,7 +153,7 @@ fatigued_page_rank_simulation_iter <- function(g, start, d, steps, PR = NULL, NF
     
     PR[start] <- PR[start] + 1
     NF[NF > 0] <- NF[NF > 0] - rep(1, length(NF[NF > 0]))
-    NF[start] <- NF[start] + 1
+    NF[start] <- nf
   }
   
   PR
@@ -165,16 +165,22 @@ fatigued_page_rank_simulation_iter <- function(g, start, d, steps, PR = NULL, NF
 # Fatigued PageRank (Power Iteration)
 #
 
-fatigued_page_rank_power_iteration <- function(g, d=0.85, eps=0.0001) {
+# For now, nf is ignored. There is instead a probability of a node being fatigued, depending on its in-degree.
+# This must be revised to also depend on the number of cycles of node fatigue (nf).
+fatigued_page_rank_power_iteration <- function(g, d=0.85, nf=10, eps=0.0001) {
   # Columns must represent outgoing links
   M <- t(as.matrix(as_adj(g)))
+  NF <- rowSums(M)
+  NF <- NF / max(NF)
+  NF <- matrix(rep(NF, length(NF)), length(NF), length(NF))
+  diag(NF) <- 0
   M <- scale(M, center=FALSE, scale=colSums(M))
   M[is.nan(M)] <- 0
   N <- vcount(g)
   v <- as.matrix(runif(N))
   v <- v / norm(v)
   last_v <- matrix(1, N) * 100
-  M_hat <- d * M + (1 - d) / N * matrix(1, N, N)
+  M_hat <- (d * M + (1-d)/N * matrix(1, N, N)) * NF
   
   iter <- 0
   repeat {
@@ -198,28 +204,34 @@ fatigued_page_rank_power_iteration <- function(g, d=0.85, eps=0.0001) {
 #
 
 #g <- make_graph("Zachary")
-g <- make_graph(c(1,2, 2,3, 3,2, 4,2, 4,3, 3,1, 4,5, 5,3, 3,6, 6,7, 7,8, 8,1, 8,3))
+#g <- make_graph(c(1,2, 2,3, 3,2, 4,2, 4,3, 3,1, 4,5, 5,3, 3,6, 6,7, 7,8, 8,1, 8,3))
+g <- make_graph(c(1,2, 2,4, 4,3, 3,2))
 
 V(g)$pr <- page_rank(g)$vector
 
 pr_sim <- page_rank_simulation(g)
 V(g)$pr_sim <- pr_sim$vector
 V(g)$pr_sim_iter <- pr_sim$iterations
+cor(V(g)$pr, V(g)$pr_sim, method="pearson")
+cor(V(g)$pr, V(g)$pr_sim, method="spearman")
 
 pr_iter <- page_rank_power_iteration(g)
 V(g)$pr_iter <- pr_iter$vector
 V(g)$pr_iter_iter <- pr_iter$iterations
+cor(V(g)$pr, V(g)$pr_iter, method="pearson")
+cor(V(g)$pr, V(g)$pr_iter, method="spearman")
 
 fpr_sim <- fatigued_page_rank_simulation(g)
 V(g)$fpr_sim <- fpr_sim$vector
 V(g)$fpr_sim_iter <- fpr_sim$iterations
-
-cor(V(g)$pr, V(g)$pr_sim, method="pearson")
-cor(V(g)$pr, V(g)$pr_sim, method="spearman")
-cor(V(g)$pr, V(g)$pr_iter, method="pearson")
-cor(V(g)$pr, V(g)$pr_iter, method="spearman")
 cor(V(g)$pr, V(g)$fpr_sim, method="pearson")
 cor(V(g)$pr, V(g)$fpr_sim, method="spearman")
+
+fpr_iter <- fatigued_page_rank_power_iteration(g)
+V(g)$fpr_iter <- fpr_iter$vector
+V(g)$fpr_iter_iter <- fpr_iter$iterations
+cor(V(g)$fpr_sim, V(g)$fpr_iter, method="pearson")
+cor(V(g)$fpr_sim, V(g)$fpr_iter, method="spearman")
 
 
 # -------------------------------------------------------------------------------------------------------------------#
