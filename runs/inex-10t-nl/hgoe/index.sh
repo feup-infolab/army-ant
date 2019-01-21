7b6956a27d5409b089e485f6f5b29fca160cedaf
José Devezas <joseluisdevezas@gmail.com>
2019-01-21 16:17:34 +0000

    Updated index output directories.

--- runs/inex-10t-nl/hgoe/index.sh
+++ runs/inex-10t-nl/hgoe/index.sh
@@ -3,5 +3,5 @@
 time ./army-ant.py index \
     --source-path "/opt/army-ant/collections/inex-2009-10t-nl/corpus" \
     --source-reader "inex_dir" \
-    --index-location "/opt/army-ant/indexes/inex-10t-nl/hgoe" \
+    --index-location "/opt/army-ant/indexes/inex-2009-10t-nl/hgoe" \
     --index-type "hgoe"
