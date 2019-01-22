89a4944849c5f70b22475dfd706243651c2b8bda
José Devezas <joseluisdevezas@gmail.com>
2019-01-22 08:22:29 +0000

    Added lucene run for INEX 52T-NL.

---
+++ runs/inex-52t-nl/lucene/index.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+time ./army-ant.py index \
+    --source-path "/opt/army-ant/collections/inex-2009-52t-nl/corpus" \
+    --source-reader "inex_dir" \
+    --index-location "/opt/army-ant/indexes/inex-2009-52t-nl/lucene" \
+    --index-type "lucene"
