9755d727e265caf62e34d02878827e29c9001a26
José Devezas <joseluisdevezas@gmail.com>
2019-02-15 11:03:47 +0000

    Added a test to explore the reversion of the relevance score given by a FeatureField.

--- external/java-impl/src/test/java/armyant/lucene/LuceneFeaturesHelperTest.java
+++ external/java-impl/src/test/java/armyant/lucene/LuceneFeaturesHelperTest.java
@@ -2,6 +2,7 @@ package armyant.lucene;
 
 import java.nio.file.Paths;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.FeatureField;
@@ -11,10 +12,12 @@ import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.MultiFields;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Bits;
 import org.testng.annotations.Test;
 
 @Test
@@ -48,5 +51,36 @@ public class LuceneFeaturesHelperTest {
             System.out.println(doc.get("doc_id"));
             System.out.println(reader.getTermVectors(docID));
         }*/
+        reader.close();
+    }
+
+    private float reverseSigmoid(float score, float w, float k, float a) {
+        return (float) Math.pow((score / w * Math.pow(k, a)) / (1 - score / w), 1 / a);
+    }
+
+    public void testRankByFeatureFields() throws Exception {
+    IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get("/tmp/lucene-index")));
+
+    IndexSearcher searcher = new IndexSearcher(reader);
+    searcher.setSimilarity(new BM25Similarity(1.2f, 0.75f));
+
+    float w = 1.8f;
+    float k = 1f;
+    float a = 0.6f;
+
+    Query query = FeatureField.newSigmoidQuery("features", "indegree", w, k, a);
+    TopDocs hits = searcher.search(query, 5);
+
+    for (int i = 0; i < hits.scoreDocs.length; i++) {
+        Document doc = searcher.doc(hits.scoreDocs[i].doc);
+        float featureValue = (float) Math.pow(
+            (hits.scoreDocs[i].score / w * Math.pow(k, a))
+            / (1 - hits.scoreDocs[i].score / w),
+            1 / a
+        );
+        System.out.println(featureValue + "\t" + StringUtils.abbreviate(doc.get("title"), "...", 80));
+    }
+
+    reader.close();
     }
 }
