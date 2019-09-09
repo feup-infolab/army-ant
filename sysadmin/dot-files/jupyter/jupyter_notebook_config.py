dd0fb9dc8dc584267b4cfb4e360be2e0ba94af25
José Devezas <joseluisdevezas@gmail.com>
2019-09-09 09:40:11 +0100

    Added configuration files to deploy Jupyter Notebook behind Nginx.

---
+++ sysadmin/dot-files/jupyter/jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+c.NotebookApp.base_url = '/jupyter'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.NotebookApp.port_retries = 50
+c.NotebookApp.trust_xheaders = True
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.allow_remote_access = True
