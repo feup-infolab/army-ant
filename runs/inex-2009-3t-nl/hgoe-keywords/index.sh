9723e0a3c2e2c4bc4b0c08251b0593161335b076
José Devezas <joseluisdevezas@gmail.com>
2019-09-20 10:29:52 +0100

    ECIR 2020 ELC: Moved the hgoe:keyword run to the correct directory.

--- runs/inex-2009-3t-nl/hgoe-keywords/index.sh
+++ runs/inex-2009-3t-nl/hgoe-keywords/index.sh
@@ -4,4 +4,4 @@ time ./army-ant.py index \
     --source-path "/opt/army-ant/collections/inex-2009-3t-nl/corpus" \
     --source-reader "inex_dir" \
     --index-location "/opt/army-ant/indexes/inex-2009-3t-nl/hgoe" \
-    --index-type "hgoe"
+    --index-type "hgoe:keywords"
---
+++ runs/inex-2009-3t-nl/hgoe-keywords/rank_concordance.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+time ./army-ant.py analysis rank-concordance \
+    --index-location "/opt/army-ant/indexes/inex-2009-3t-nl/hgoe" \
+    --index-type "hgoe" \
+    --rw-length "2,3,4" \
+    --rw-repeats "100,1000,2500,10000" \
+    --topics-path "/opt/army-ant/collections/inex-2009-3t-nl/topics/2010-topics.xml" \
+    --output-path "/opt/army-ant/analysis/inex_2009_3t_nl-hgoe_rws-rank_concordance" \
+    --cutoff 1000 \
+    --repeats 100 \
+    --method "kendall_w"
