73756d4e1688b2b94a301a89f0d10bef5b1140b0
José Devezas <joseluisdevezas@gmail.com>
2019-09-24 14:43:53 +0100

    ECIR 2020 ELC: Corrected path to INEX 2009 corpus data.

--- runs/inex-2009/null_index/index.sh
+++ runs/inex-2009/null_index/index.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 time ./army-ant.py index \
-    --source-path "/opt/army-ant/collections/inex-2009-10t-nl/corpus" \
+    --source-path "/opt/army-ant/collections/inex-2009/corpus" \
     --source-reader "inex_dir" \
     --index-location "/tmp" \
     --index-type "null_index" \
