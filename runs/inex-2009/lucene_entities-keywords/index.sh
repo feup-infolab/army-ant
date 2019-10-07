e5e02036534e1275a4258a7d40603bca4246d8bd
José Devezas <joseluisdevezas@gmail.com>
2019-10-07 07:50:11 +0100

    ECIR 2020 GM: Fixed error with the target directory for the index.

--- runs/inex-2009/lucene_entities-keywords/index.sh
+++ runs/inex-2009/lucene_entities-keywords/index.sh
@@ -3,5 +3,5 @@
 time ./army-ant.py index \
     --source-path "/opt/army-ant/collections/inex-2009/corpus" \
     --source-reader "inex_dir" \
-    --index-location "/opt/army-ant/indexes/inex-2009-3t-nl/lucene_entities-keywords" \
+    --index-location "/opt/army-ant/indexes/inex-2009/lucene_entities-keywords" \
     --index-type "lucene_entities:keywords"
