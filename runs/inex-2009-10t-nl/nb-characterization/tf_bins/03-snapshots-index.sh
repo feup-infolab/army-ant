8a980050ce08e92af9bdd8f9f5b30b94dd04c5f7
José Devezas <joseluisdevezas@gmail.com>
2020-01-23 16:22:19 +0000

    Fixed a bug with the extra arguments.

--- runs/inex-2009-10t-nl/nb-characterization/tf_bins/03-snapshots-index.sh
+++ runs/inex-2009-10t-nl/nb-characterization/tf_bins/03-snapshots-index.sh
@@ -17,7 +17,7 @@ do
     snapshots_num_docs="1 2 3 4 5 10 25 50 100 1000 2000 3000 5000 8000"
     extra_args="--features-location $config_dir"
 
-    snapshots_index $source_path $index_path $index_type "$snapshots_num_docs" $extra_args
+    snapshots_index $source_path $index_path $index_type "$snapshots_num_docs" "$extra_args"
 done
 
 rm -rf "$features_path"
