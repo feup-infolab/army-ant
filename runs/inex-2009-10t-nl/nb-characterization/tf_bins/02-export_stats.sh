533a88e1f4a26610c6220b25cf0002544320eaf2
José Devezas <joseluisdevezas@gmail.com>
2020-01-24 11:44:59 +0000

    Fixed a bug with the bins size.

--- runs/inex-2009-10t-nl/nb-characterization/tf_bins/02-export_stats.sh
+++ runs/inex-2009-10t-nl/nb-characterization/tf_bins/02-export_stats.sh
@@ -8,7 +8,7 @@ for bin_size in $bin_sizes
 do
     index_path="/opt/army-ant/indexes/inex-2009-10t-nl/hgoe-tf_bins_$bin_size"
     index_type="hgoe:tf_bins"
-    base_outdir="/opt/army-ant/analysis/inex_2009_10t_nl-tf_bins_$bin-size-stats"
+    base_outdir="/opt/army-ant/analysis/inex_2009_10t_nl-tf_bins_$bin_size-stats"
 
     export_stats $index_path $index_type $base_outdir
 done
