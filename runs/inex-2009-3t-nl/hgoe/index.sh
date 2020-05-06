4aee88cef70f1a5e09517c5e3849d3e6a6005d18
José Devezas <joseluisdevezas@gmail.com>
2020-05-06 17:26:29 +0100

    Checkpoint.

--- runs/inex-2009-3t-nl/hgoe/index.sh
+++ runs/inex-2009-3t-nl/hgoe/index.sh
@@ -4,6 +4,4 @@ time ./army-ant.py index \
     --source-path "/opt/army-ant/collections/inex-2009-3t-nl/corpus" \
     --source-reader "inex_dir" \
     --index-location "/opt/army-ant/indexes/inex-2009-3t-nl/hgoe-keywords" \
-    --index-type "hgoe:keywords" \
-    #--index-type "hgoe:tf_bins" \
-    #--features-location "/opt/army-ant/features/inex_2009_3t_nl"
+    --index-type "hgoe"
--- scripts/mongo_objectid_to_date.sh
+++ scripts/mongo_objectid_to_date.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+if [ $# -lt 1 ]
+then
+  echo Usage: $0 OBJECTID
+  exit 1
+fi
+
 date=$(mongo --quiet --eval "new ObjectId('$1').getTimestamp().toISOString()")
 (echo -n $date | xclip -selection c) && echo "$1 has ISO date $date (copied to clipboard)"
