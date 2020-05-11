efb8225f1259e4fed3ade351b63fa3befd140d8b
José Devezas <joseluisdevezas@gmail.com>
2020-05-11 10:48:52 +0100

    Reorganized the configuration file naming to reflect indexing parameters and added missing indexes for last experiments.

--- runs/inex-2009/hgoe-keywords/04-index-tf_bins.sh
+++ runs/inex-2009/hgoe-keywords/04-index-tf_bins.sh
@@ -3,6 +3,6 @@
 time ./army-ant.py index \
     --source-path "/opt/army-ant/collections/inex-2009/corpus" \
     --source-reader "inex_dir" \
-    --index-location "/opt/army-ant/indexes/inex-2009/hgoe-tf_bins" \
+    --index-location "/opt/army-ant/indexes/inex-2009/hgoe-keywords-tf_bins" \
     --index-type "hgoe:keywords:tf_bins" \
     --features-location "/opt/army-ant/features/inex_2009"
